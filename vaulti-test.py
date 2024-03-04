cfacd50e1a820a4a96b92ef00a6dee4abf36c133
shir-git <142160525+shir-git@users.noreply.github.com>
2024-03-04 13:32:05 +0200

    Update vaulti-test.py

--- vaulti-test.py
+++ vaulti-test.py
@@ -3,16 +3,16 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 from airflow.hooks.base_hook import BaseHook
 
-
+mayaya = Variable.get("mayaya")
 def get_secrets(**kwargs):
-    conn = BaseHook.get_connection(kwargs['vaulti'])
+    # conn = BaseHook.get_connection(kwargs['vaulti'])
+    conn = BaseHook.get_connection('vaulti')
     print(f"Password: {conn.password}, Login: {conn.login}, URI: {conn.get_uri()}, Host: {conn.host}")
 
 with DAG('example_secrets_dags', start_date=datetime(2020, 1, 1), schedule_interval=None) as dag:
 
-
     test_task = PythonOperator(
         task_id='test-task',
         python_callable=get_secrets,
-        op_kwargs={'my_conn_id': 'smtp_default'},
+        op_kwargs={'my_conn_id': 'vaulti'},
     )
